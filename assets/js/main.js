d95270ee61c0caf81ca52cb69ee6f0f17175bc73
Scifiron <scifiron@protonmail.com>
2021-06-03 13:29:28 -0300

    Deployed to ropsten test network.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,18 @@
 const provider = new ethers.providers.Web3Provider(window.ethereum);
 const signer = provider.getSigner()
 
+const contractAddr = {
+    '31337': '0xB7f8BC63BbcaD18155201308C8f3540b07f84F5e',
+    '3': '0x57a2Ab3407768d6adA59293da0477951aac1b99C'
+};
 
-const greetingAddr = '0xB7f8BC63BbcaD18155201308C8f3540b07f84F5e';
 const greetingAbi = [{"inputs":[{"internalType":"string","name":"_intialGreeting","type":"string"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"string","name":"newGreeting","type":"string"}],"name":"UpdatedGreeting","type":"event"},{"inputs":[],"name":"greeting","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"_newGreeting","type":"string"}],"name":"setGreeting","outputs":[],"stateMutability":"nonpayable","type":"function"}];
 
-const greetingContract = new ethers.Contract(greetingAddr, greetingAbi, provider);
 
 window.onload = async function() {
+    chainId = (await provider.detectNetwork()).chainId;
+    greetingContract = new ethers.Contract(contractAddr[chainId], greetingAbi, provider);
+
     document.getElementById('newGreeting').onkeydown = function(e){
         if(e.keyCode == 13){
             changeGreeting();
@@ -32,7 +37,7 @@ async function fetchGreeting() {
 async function changeGreeting(form) {
     let newGreeting = document.getElementById('newGreeting').value;
 
-    const greetingContract = new ethers.Contract(greetingAddr, greetingAbi, signer);
+    const greetingContract = new ethers.Contract(contractAddr[chainId], greetingAbi, signer);
     const transaction = await (await greetingContract.setGreeting(newGreeting)).wait();
     fetchGreeting();
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -5,4 +5,13 @@ require("@nomiclabs/hardhat-waffle");
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+      hardhat: {
+          chainId: 31337
+      },
+      ropsten: {
+          url: process.env.ROPSTEN_URL,
+          accounts: [ process.env.ROPSTEN_PK ]
+      }
+  }
 };
