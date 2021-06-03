3f94ba102cca893155c3f2989b0143c80c060b55
Scifiron <scifiron@protonmail.com>
2021-06-03 16:44:31 -0300

    Displaying error when metamask is not installed.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,3 @@
-const provider = new ethers.providers.Web3Provider(window.ethereum);
-const signer = provider.getSigner()
 
 const contractAddr = {
     '31337': '0xB7f8BC63BbcaD18155201308C8f3540b07f84F5e',
@@ -8,25 +6,15 @@ const contractAddr = {
 
 const greetingAbi = [{"inputs":[{"internalType":"string","name":"_intialGreeting","type":"string"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"string","name":"newGreeting","type":"string"}],"name":"UpdatedGreeting","type":"event"},{"inputs":[],"name":"greeting","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"_newGreeting","type":"string"}],"name":"setGreeting","outputs":[],"stateMutability":"nonpayable","type":"function"}];
 
-
-window.onload = async function() {
-    chainId = (await provider.detectNetwork()).chainId;
-    greetingContract = new ethers.Contract(contractAddr[chainId], greetingAbi, provider);
-
-    document.getElementById('newGreeting').onkeydown = function(e){
-        if(e.keyCode == 13){
-            changeGreeting();
-        }
-    };
-
-    await window.ethereum.request({ method: 'eth_requestAccounts' });
-    await fetchGreeting();
-    greetingContract.on('UpdatedGreeting', updateGreeting);
-}
-
 function updateGreeting(newGreeting) {
     let greetingElement = document.getElementById('greeting');
     greetingElement.innerText = newGreeting;
+}
+
+function setError(message) {
+    let greetingElement = document.getElementById('greeting');
+    greetingElement.classList.add('error');
+    greetingElement.innerText = message;
 }
 
 async function fetchGreeting() {
@@ -41,3 +29,29 @@ async function changeGreeting(form) {
     const transaction = await (await greetingContract.setGreeting(newGreeting)).wait();
     fetchGreeting();
 }
+
+window.onload = async function() {
+    if( !window.ethereum ) {
+        setError("No provider found, please install metamask.");
+    } else {
+        document.getElementById('greetingSetter').classList.remove('hidden');
+        
+        provider = new ethers.providers.Web3Provider(window.ethereum);
+        signer = provider.getSigner()
+
+        chainId = (await provider.detectNetwork()).chainId;
+        greetingContract = new ethers.Contract(contractAddr[chainId], greetingAbi, provider);
+
+        document.getElementById('newGreeting').onkeydown = function(e){
+            if(e.keyCode == 13){
+                changeGreeting();
+            }
+        };
+
+        await window.ethereum.request({ method: 'eth_requestAccounts' });
+        await fetchGreeting();
+        greetingContract.on('UpdatedGreeting', updateGreeting);
+    }
+}
+
+
